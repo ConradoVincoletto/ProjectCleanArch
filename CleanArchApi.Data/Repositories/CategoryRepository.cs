267e991dc08cec140ae0de009bf624b354f084db
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-07-12 20:12:13 -0300

    feat: CategoryRepository adicionado

---
+++ CleanArchApi.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanArch.Data.Context;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Data.Repositories
+{
+    public class CategoryRepository: ICategoryRespository
+    {
+        ApplicationDbContext _categoryContext;
+
+        public CategoryRepository(ApplicationDbContext categoryContext)
+        {
+            _categoryContext = categoryContext;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id) 
+            => await _categoryContext.Categories.FindAsync(id);
+
+        public async Task<IEnumerable<Category>> GetCategories() 
+            => await _categoryContext.Categories.ToListAsync();
+
+        public async Task<Category> Remove(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
