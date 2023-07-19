19196c3eb1c76439ddd1332e09038584b4b71c62
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-07-19 19:55:10 -0300

    feat: Service da Category

---
+++ CleanArchApi.Application/Services/CategoryService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRespository _categoryRespository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRespository categoryRespository, IMapper mapper)
+        {
+            _categoryRespository = categoryRespository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRespository.Create(categoryEntity);
+        }
+
+        public async Task<CategoryDTO> GetById(int? id)
+        {
+            var categoryEntity = await _categoryRespository.GetById(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntity = await _categoryRespository.GetCategories();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var categoryEntity = _categoryRespository.GetById(id).Result;
+            await _categoryRespository.Remove(categoryEntity);
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRespository.Update(categoryEntity);
+        }
+    }
+}
