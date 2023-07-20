560ee2e5451d31be5cbef0f935296d9a2c5a1507
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-07-20 18:21:13 -0300

    feat: configuração na program

--- WebApiIdentity/Program.cs
+++ WebApiIdentity/Program.cs
@@ -1,14 +1,21 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using WebApiIdentity.Context;
+using WebApiIdentity.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new AuthorizeFilter());
+});
 
-
- //Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -22,6 +29,29 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 7;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(optionts =>
+    {
+        optionts.Cookie.Name = "AspnetCore.Cookies";
+        optionts.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+        optionts.SlidingExpiration = true;
+    });
+
+builder.Services.AddScoped<ISeedUserRolesInitial, SeedUserRolesInitial>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequiredUserAdminGerenteRoles",
+        policy => policy.RequireRole("User", "Admin", "Gerente"));
+});
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
@@ -33,9 +63,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+await CriarPerfisUsuariosAsync(app);
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+async Task CriarPerfisUsuariosAsync(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRolesInitial>();
+        await service.SeedRolesAsync();
+        await service.SeedUserAsync(); 
+    }
+}
