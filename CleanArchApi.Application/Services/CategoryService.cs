a9e11840304353cafec6f49d1889d008ac590fe2
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-07-20 12:36:09 -0300

    feat: aplicação de estudos do Identity, Autorização e Autenticação

--- CleanArchApi.Application/Services/CategoryService.cs
+++ CleanArchApi.Application/Services/CategoryService.cs
@@ -3,11 +3,6 @@ using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
 using CleanArch.Domain.Entities;
 using CleanArch.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Application.Services
 {
---
+++ WebApiIdentity/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiIdentity.Entities;
+
+namespace WebApiIdentity.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Aluno>().HasData(
+            new Aluno
+            {
+                AlunoId = 1,
+                Nome = "Conrado",
+                Email = "conrado@example.com",
+                Idade = 32,
+                Curso = "Math"
+            });
+        }
+    }
+}
