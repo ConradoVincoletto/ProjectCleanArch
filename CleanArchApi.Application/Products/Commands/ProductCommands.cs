ec51979cade2813ef56d741a509c0b3c6ff8da8f
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-08-01 20:15:14 -0300

    feat: adicionado as Query e Commands

---
+++ CleanArchApi.Application/Products/Commands/ProductCommands.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Products.Commands
+{
+    public abstract class ProductCommands : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        public int CategoryId { get; private set; }
+    }
+}
