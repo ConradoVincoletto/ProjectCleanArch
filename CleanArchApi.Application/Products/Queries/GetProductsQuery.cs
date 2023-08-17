e1114c1b262fa878e498470c596b7b0c5bdf4202
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-08-17 20:11:42 -0300

    feat: adicionando classe do handler

--- CleanArchApi.Application/Products/Queries/GetProductsQuery.cs
+++ CleanArchApi.Application/Products/Queries/GetProductsQuery.cs
@@ -1,11 +1,5 @@
 ﻿using CleanArch.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Application.Products.Queries
 {
--- CleanArchApi.Domain/Entities/Product.cs
+++ CleanArchApi.Domain/Entities/Product.cs
@@ -10,13 +10,13 @@ namespace CleanArch.Domain.Entities
     public sealed class Product : Entity
     {
         public int Id { get; set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
+        public string Name { get;  set; }
+        public string Description { get;  set; }
         public decimal Price { get; set; }
-        public int Stock { get; private set; }
-        public string Image { get; private set; }
-        public int CategoryId { get; private set; }
-        public Category Category { get; private set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
         public Product(string name, string description, decimal price, int stock, string image)
         {
