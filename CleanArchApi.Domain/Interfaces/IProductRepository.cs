4f7b605ee4841a34aa531655073ab79c679d19be
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-09-18 20:21:24 -0300

    fix: alteração do tipo de tarefa na interface do Repositório.

--- CleanArchApi.Domain/Interfaces/IProductRepository.cs
+++ CleanArchApi.Domain/Interfaces/IProductRepository.cs
@@ -10,7 +10,7 @@ namespace CleanArch.Domain.Interfaces
     public interface IProductRepository
     {
         Task<Product> GetProductCategoryAsync(int? id);
-        Task<Product> GetProduct();
+        Task<IEnumerable<Product>> GetProductAsync();
         Task<Product> GetByIdAsync(int? id);
         Task<Product> CreateAsync(Product product);
         Task<Product> UpdateAsync(Product product);
