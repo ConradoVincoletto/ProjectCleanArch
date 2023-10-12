1fcda4f165205870fca2fb884464eb86fb8a176f
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-10-12 17:00:47 -0300

    feat: migração da tabela do identity

--- CleanArchApi.Data/Migrations/20231012195854_AddIdentityTables.Designer.cs
+++ CleanArchApi.Data/Migrations/20231012195854_AddIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using CleanArch.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArch.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231012195854_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
