cdf33a39bfa3d3e9f85ec68f961264af005467fd
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-12-11 19:33:33 -0300

    feat: criação dos models para criar o controlador para fazer o login e o resgistro do usuário.

---
+++ CleanArch.WebApi/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebApi.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is requerid")]
+        [EmailAddress(ErrorMessage = "Invalid Format Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is requerid")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
