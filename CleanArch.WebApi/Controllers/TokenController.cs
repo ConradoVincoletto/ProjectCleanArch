52985f2be8b0316798523ea588691642ce4aa709
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-12-12 19:43:08 -0300

    feat: construção da controller para gerar o token

---
+++ CleanArch.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,98 @@
+﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanArchMvc.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
+        {
+            _authenticate = authenticate;
+            _configuration = configuration;
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authenticate.RegisterUser(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                return Ok($"User {userInfo.Email} was create sucessfully.");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPost("LoginsUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                return GenerateToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return BadRequest(ModelState);
+            }
+                
+        }
+
+        private UserToken GenerateToken(LoginModel userinfo)
+        {
+            var claims = new[]
+            {
+                //declaração do usário
+                new Claim("email", userinfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+
+            };
+
+            //gerar chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            //gerar a assinatura digital do token
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //tempor de expiração do token
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerar o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                //emissor
+                issuer: _configuration["Jwt:Issuer"],
+                //audiance
+                audience: _configuration["Jwt:Audience"],
+                //reivindicação
+                claims: claims,
+                //data da expiração
+                expires: expiration,
+                //assinatura digital
+                signingCredentials: credentials
+                );
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+
+        }
+    }
+}
