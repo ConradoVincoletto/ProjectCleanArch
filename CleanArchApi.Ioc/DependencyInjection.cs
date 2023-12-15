98a01848501f743341d9772e2c38cb7fe79d61ee
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-12-15 15:34:20 -0300

    fix: ajustes na camada IoC

--- CleanArchApi.Ioc/DependencyInjection.cs
+++ CleanArchApi.Ioc/DependencyInjection.cs
@@ -13,49 +13,48 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiIdentity.Services;
 
-namespace CleanArch.Ioc
+namespace CleanArch.Ioc;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+
+        //TODO: adicionar a sua string do sql
+        services.AddDbContext<ApplicationDbContext>(opitions =>
+        opitions.UseSqlServer(configuration.GetConnectionString("Data Source=LAPTOP-MGPDKJRC\\SQLSERVER2022;Initial Catalog=CLEAN_ARQCH_UDEMY;Integrated Security=True;Encrypt=False;TrustServerCertificate=False"),
+        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
         {
+            options.Password.RequiredLength = 7;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredUniqueChars = 1;
+        });
 
-            //TODO: adicionar a sua string do sql
-            services.AddDbContext<ApplicationDbContext>(opitions =>
-            opitions.UseSqlServer(configuration.GetConnectionString("Data Source=LAPTOP-MGPDKJRC\\SQLSERVER2022;Initial Catalog=CLEAN_ARQCH_UDEMY;Integrated Security=True;Encrypt=False;TrustServerCertificate=False"),
-            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("RequiredUserAdminGerenteRoles",
+                policy => policy.RequireRole("User", "Admin", "Gerente"));
+        });
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+        services.AddScoped<ICategoryRespository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
 
-            services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequiredLength = 7;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredUniqueChars = 1;
-            });
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("RequiredUserAdminGerenteRoles",
-                    policy => policy.RequireRole("User", "Admin", "Gerente"));
-            });
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
-            services.AddScoped<ICategoryRespository, CategoryRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<ICategoryService, CategoryService>();
+        var myHandlers = AppDomain.CurrentDomain.Load("CleanArch.Application");
+        services.AddMediatR(cfc => cfc.RegisterServicesFromAssemblies(myHandlers));
 
-            services.AddScoped<IAuthenticate, AuthenticateService>();
-            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
-
-            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
-
-            var myHandlers = AppDomain.CurrentDomain.Load("CleanArch.Application");
-            services.AddMediatR(cfc => cfc.RegisterServicesFromAssemblies(myHandlers));
-
-            return services;
-        }
+        return services;
     }
 }
--- CleanArchApi.Ioc/DependencyinjectionSwagger.cs
+++ CleanArchApi.Ioc/DependencyinjectionSwagger.cs
@@ -1,48 +1,41 @@
-﻿using Microsoft.AspNetCore.Http.Connections;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CleanArch.Ioc
+namespace CleanArch.Ioc;
+
+public static class DependencyinjectionSwagger
 {
-    public static class DependencyinjectionSwagger
+    public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
     {
-        public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+        services.AddSwaggerGen(c =>
         {
-            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchApi", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "CleanArchApi", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
-                {
-                    //definir configuração
-                    Name = "Authorization",
-                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-                    Description = "JWT Authorization é preciso mencionar a palavra Bearer na frente do token."
-                });
-                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-
-                });
+                //definir configuração
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization é preciso mencionar a palavra Bearer na frente do token."
             });
-            return services;
-        }
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+
+            });
+        });
+        return services;
     }
 }
